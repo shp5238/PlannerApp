ba4197aab7c52fb5a086ab84b0cefbdbf9b337d7
Shreya <shreyapasupuleti@gmail.com>
2025-04-18 17:17:48 -0700

    Initial Commit

---
+++ Planner!/Planner_App.swift
@@ -0,0 +1,17 @@
+//
+//  Planner_App.swift
+//  Planner!
+//
+//  Created by Shreya Pasupuleti on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Planner_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Planner!UITests/Planner_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Planner_UITestsLaunchTests.swift
+//  Planner!UITests
+//
+//  Created by Shreya Pasupuleti on 4/18/25.
+//
+
+import XCTest
+
+final class Planner_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
